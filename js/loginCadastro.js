4fdc6929821bc5ef1aee6491856112d6b02f9b1e
pietroabrahamian <pietroabrahamian2018@gmail.com>
2025-05-17 22:34:04 -0300

    loginCadastro.js - aparecer e esconder card de cadastro e login

--- js/loginCadastro.js
+++ js/loginCadastro.js
@@ -1,4 +1,22 @@
-const voltar = document.getElementById("voltar");
-voltar.addEventListener("click", ()=>{
-    window.location.href = "../bemvindo/bemvindo.html";
+const irPCadastro = document.getElementById("irPCadastro");
+const irPLogin = document.getElementById("irPLogin");
+const formLogin = document.getElementById("formLogin");
+const formCadastro = document.getElementById("formCadastro");
+
+irPCadastro.addEventListener("click", ()=>{
+    formLogin.style.display = "none";
+    formCadastro.style.display = "flex";
+});
+
+irPLogin.addEventListener("click", ()=>{
+    formLogin.style.display = "flex";
+    formCadastro.style.display = "none";
+});
+
+const buttonLogin = document.getElementById("buttonLogin");
+const buttonCadastro = document.getElementById("buttonCadastro");
+
+buttonCadastro.addEventListener("click", ()=>{
+    console.log("cadastro");
+    window.location.href = "../../Index.html";
 });
