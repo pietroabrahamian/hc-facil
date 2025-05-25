e3b9ad04fc0ff81c35c4718ddb5634f03eacb05f
pietroabrahamian <pietroabrahamian2018@gmail.com>
2025-05-25 00:33:03 -0300

    links do index corrigidos e header das home-pages corrigidos.

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const marqueSuaConsulta = document.getElementById("marqueSuaConsulta");
+const facaSeuLogin = document.getElementById("facaSeuLogin");
+const esqueciSenha = document.getElementById("esqueciSenha");
+const videos = document.getElementById("videos");
+
+marqueSuaConsulta.addEventListener("click", ()=>{
+    window.location.href = "./assets/home-pages/marque-sua-consulta/marque-sua-consulta.html";
+});
+facaSeuLogin.addEventListener("click", ()=>{
+    window.location.href = "./assets/home-pages/faca-seu-login/faca-seu-login.html";
+});
+esqueciSenha.addEventListener("click", ()=>{
+    window.location.href = "./assets/home-pages/esqueci-senha/esqueci-senha.html";
+});
+videos.addEventListener("click", ()=>{
+    window.location.href = "";
+});
